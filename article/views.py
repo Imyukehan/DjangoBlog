7666da64ad3a93b36d6bd5501e2735b0b3cd891c
Khan <njyukehan@gmail.com>
2017-08-17 18:21:06 +0800

    add pagination

--- article/views.py
+++ article/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 # Show homepage
 def home(request):
-    post_list = Article.objects.all()
+    posts = Article.objects.all()
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage: 
+        post_list = paginator.paginator(paginator.num_pages)
     return render(request, 'home.html', {'post_list': post_list})
 # Show article
 def detail(request, id):
@@ -31,3 +41,19 @@ def search_tag(request, tag):
     except Article.DoesNotExist:
         raise Http404
     return render(request, 'tag.html', {'post_list': post_list})
+
+# add search function
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains = s)
+            if len(post_list) == 0 :
+                return render(request,'archive.html', {'post_list' : post_list,
+                                                    'error' : True})
+            else :
+                return render(request,'archive.html', {'post_list' : post_list,
+                                                    'error' : False})
+    return redirect('/')
